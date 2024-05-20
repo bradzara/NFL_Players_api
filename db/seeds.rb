0beec8b80ec3c5daa385f1069b5bfa6481ee4001
bradzara <bradzara93@gmail.com>
2024-05-19 21:52:42 -0700

    Add model with seeds for initial data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+player = Player.new(name: "Justin Jefferson", team: "Minnesota Vikings", position: "Wide Receiver")
+player.save
+
+player = Player.new(name: "Jalen Hurts", team: "Philadelphia Eagles", position: "Quarterback")
+player.save
